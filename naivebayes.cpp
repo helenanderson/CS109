8055a29ff8e3a5b2c92c71cb4e699ec24e4d8ec4
helenanderson <helenanderson@stanford.edu>
2014-12-03 22:32:29 -0800

    working on file reading

--- naivebayes.cpp
+++ naivebayes.cpp
@@ -2,28 +2,51 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <stack>
 
 
 using namespace std;
 
-int main() {
+void readTrainingData(string filename) {
+//vector<vector<vector<int>>> readTrainingData(string filename){
 
 	string line;
 	int variableNum;
 	int vectorNum;
-	ifstream trainfile ("simple-train.txt");
-	if (trainfile.is_open()) {
+	ifstream trainfile (filename);
+
+	getline(trainfile,line);
+	variableNum = stoi(line);
+	//cout << variableNum << endl;
+	getline(trainfile,line);
+	vectorNum = stoi(line);
+	//cout << vectorNum << endl;
+	
+	vector<vector<vector<int> > > trainingData;
+
+	for (int i = 0; i < vectorNum; i++) {
 		getline(trainfile,line);
-		variableNum = stoi(line);
-		cout << variableNum << endl;
-		getline(trainfile,line);
-		vectorNum = stoi(line);
-		cout << vectorNum << endl;
-		trainfile.close();
+		stack<int> reversedData;
+		string buffer;
+		stringstream ss(line);
+		while (ss >> buffer) {
+			int value = stoi(buffer);
+			reversedData.push(value);
+		}
+		while (reversedData.size() > 0) {
+			int check = reversedData.top();
+			reversedData.pop();
+		}	
 	}
 
-	else cout << "Unable to open file." << endl;
+	trainfile.close();
+	return;
+}
 
+int main() {
+
+	readTrainingData("heart-train.txt");
 	return 0;
 
 }
