22f05fd98937c4d72a43aeef317a2210a6e58e92
helenanderson <helenanderson@stanford.edu>
2014-12-04 17:55:05 -0800

    heart test is scary accurate

--- logreg.cpp
+++ logreg.cpp
@@ -15,53 +15,86 @@ using namespace std;
 vector<double> train(string filename) {
 
 	string line;
-	int variableNum;
-	int vectorNum;
 	ifstream trainfile (filename);
 
 	getline(trainfile,line);
-	variableNum = stoi(line);
+	int variableNum = stoi(line);
 	getline(trainfile,line);
-	vectorNum = stoi(line);
+	int vectorNum = stoi(line);
 
 	vector<double> betas(variableNum + 1);
 	for (int i = 0; i < betas.size(); i++) {
 		betas[i] = 0.0;
+	} 
+
+	//read in data
+	vector<double> allYs;
+	vector<vector<double> > alldata; 
+	for (int i = 0; i < vectorNum; i++) {
+		vector<double> onedata;
+		alldata.push_back(onedata);
+		alldata[i].push_back(1.0);		
+		getline(trainfile,line);
+		string buffer;
+		stringstream ss(line);
+		for (int k = 0; k < variableNum; k++) {
+			ss >> buffer; 
+			int value = stoi(buffer);
+			alldata[i].push_back(value);
+
+		}
+		ss >> buffer;
+		int y = stoi(buffer);
+		allYs.push_back(y);
+
 	}
+/*
+	for (int i = 0; i < alldata.size(); i++) {
+		for (int j = 0; j < alldata[0].size(); j++) {
+			cout << alldata[i][j];
+		}
+		cout << endl;
+	}
+
+	for (int i = 0 ; i < allYs.size(); i++) {
+		cout << allYs[i] << " ";
+	}
+	cout << endl; 
+
+	*/
+
 	for (int e = 0; e < EPOCHS; e++) {
 
 		vector<double> gradients(variableNum + 1);
-		for (int i = 0; i < gradients.size(); i++) {
-			gradients[i] = 0.0;
-		}
-		double z; 
-		for (int k = 0; k <= variableNum; k++) {
-			for (int i = 1; i <= vectorNum; i++) {
 
-				vector<int> data;
-				data.push_back(1);
-				z += betas[0];
-				getline(trainfile,line);
-				string buffer;
-				stringstream ss(line);
-				for (int j = 1; j <= variableNum; j++) {
-					ss >> buffer; 
-					int value = stoi(buffer);
-					data.push_back(value);
-					z += betas[j]*value;
-				}
-				ss >> buffer;
-				int y = stoi(buffer);
-
-				gradients[k] += data[k]*(y - (1/(1 + pow(M_E, -z))));			
+		//cache z's
+		vector<double> zVals;
+		for (int i = 0; i < vectorNum; i++) {
+			double z = 0.0;
+			zVals.push_back(z);
+			for (int j = 0; j < variableNum + 1; j++) {
+				double toAdd = betas[j]*alldata[i][j];
+				zVals[i] += toAdd;
 			}
 		}
+		
+
+		for (int k = 0; k <= variableNum; k++) {
+			double gradient = 0.0;
+			gradients.push_back(gradient);
+			for (int i = 0; i < vectorNum; i++) {
+				gradients[k] += alldata[i][k]*(allYs[i] - (1/(1 + pow(M_E, -zVals[i]))));
+
+			}
+		}
+		
 		for (int k = 0; k <= variableNum; k++) {
 			betas[k] += LEARN_RATE*gradients[k];
 		}
 	}
 	return betas;
 }
+
 
 void test(string filename, vector<double> trainData) {	
 
@@ -81,7 +114,7 @@ void test(string filename, vector<double> trainData) {
 	double z = 0.0;
 
 	for (int i = 1; i <= vectorNum; i++) {
-		vector<int> data;
+		vector<double> data;
 		data.push_back(1);
 		z += trainData[0];
 		getline(testfile,line);
@@ -98,6 +131,7 @@ void test(string filename, vector<double> trainData) {
 		classcount[y]++;
 
 		double p = (1/(1 + pow(M_E, -z)));
+		cout << p << endl;
 		int yGuess = 0;
 		if (p > 0.5) {
 			yGuess = 1;
@@ -121,8 +155,8 @@ int main() {
 	//vector<double> voteVector = train("vote-train.txt");
 	//test("vote-test.txt", voteVector);
 
-	//vector<double> heartVector = train("heart-train.txt");
-	//test("heart-test.txt", heartVector);
+	vector<double> heartVector = train("heart-train.txt");
+	test("heart-test.txt", heartVector);
 
 	return 0;
 }
